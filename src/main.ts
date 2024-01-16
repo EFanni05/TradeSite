4edc9bd52a7922bb0d8a5dafd6c5390c2b56d633
Fannichan <elekes.fanni2005@gmail.com>
2024-01-16 10:57:06 +0100

    shippimg calculator changes

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ function Calculate(){
     liRegister.textContent = ""
     try{
       let vs:Data = new Data(country, parseInt(numberOfPc))
-      let weight:number = vs.pcs * 0.028 
+      let weight:number = vs.pcs * 0.28 
       if(checker(vs.name)){
         if(weight < 0.5){
           if(vs.name == "Hungary"){
